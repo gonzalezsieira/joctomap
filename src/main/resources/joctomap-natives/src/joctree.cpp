a6d025d26fb762019d998b0d799222f57eef56ce
Adrián González Sieira <adrian.gonzalez@usc.es>
2014-04-07 13:20:55 +0200

    Removed prints from native methods

--- src/main/resources/joctomap-natives/src/joctree.cpp
+++ src/main/resources/joctomap-natives/src/joctree.cpp
@@ -1,5 +1,8 @@
-#include  <iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include <jni.h>
+#include <octomap/OcTree.h>
 #include <octomap/AbstractOcTree.h>
 #include <octomap/OccupancyOcTreeBase.h>
 #include "joctree.h"
@@ -17,7 +20,7 @@ JNIEXPORT jboolean JNICALL Java_es_usc_citius_lab_joctomap_JOctree_write
 	jboolean iscopy = false;
 	const char *nativeFilename = env->GetStringUTFChars(filename, &iscopy);
 	//write octree in filename
-	AbstractOcTree *octree = (AbstractOcTree*) octreePointer;
+	OcTree *octree = (OcTree*) octreePointer;
 	bool value = octree->write(nativeFilename);
 	//release memory of native char*
 	env->ReleaseStringUTFChars(filename, nativeFilename);
@@ -33,10 +36,46 @@ JNIEXPORT jlong JNICALL Java_es_usc_citius_lab_joctomap_JOctree_read
   (JNIEnv *env, jobject obj, jstring filename){
 	//convert jstring into native char*
 	jboolean iscopy = false;
+	/*
+	 * This workaround to read an Octree comes from an error using
+	 * AbstractOctree::read(filename). The library is unable
+	 * to return an Octree of the type specified in the file (.ot), so
+	 * the implementation of Octree::readData(stream) must be used to
+	 * avoid using the factory above./
+	 */
+	//a new octree is initialized with a default resolution
+	OcTree *ot = new OcTree(0.1);
+	//convert jstring to char*
 	const char *nativeFilename = env->GetStringUTFChars(filename, &iscopy);
-	//read octree in the given filename
-	AbstractOcTree *octree = AbstractOcTree::read(nativeFilename);
-	//release memory of native char*
+	//open input stream to read data
+	ifstream stream; stream.open(nativeFilename);
+	//release memory of jstring
 	env->ReleaseStringUTFChars(filename, nativeFilename);
-	return (jlong) octree;
+	//array to store the current line of the file
+	char line[250];
+	bool stop = false;
+	//read the first lines of the file that start with "#"
+	while(!stop){
+		//read current line
+		stream.getline(line, 256);
+		string line_string = line;
+		//modify the octree resolution when the "res" line is read
+		if(line_string.substr(0, 3).compare("res") == 0){
+			//read resolution using istringstream (to avoid locale conflicts -
+			//different symbols to separate decimals.
+			double res; istringstream istr(line_string.substr(4).c_str());
+			istr >> res;
+			//modify octree resolution
+			ot->setResolution(res);
+		}
+		//stop when the "data" line is read
+		if(line_string.compare("data") == 0){
+			stop = true;
+		}
+	}
+	//read data in the current location of the input stream
+	ot->readData(stream);
+	//close input stream
+	stream.close();
+	return (jlong) ot;
 }
