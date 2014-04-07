99ef4a81243c1830d4d63f2cadd9db5dec7108a5
Adrián González Sieira <adrian.gonzalez@usc.es>
2014-04-07 13:47:44 +0200

    Modified JUnit write test to check content of file

--- src/test/java/es/usc/citius/lab/joctomap/JOctreeIOTest.java
+++ src/test/java/es/usc/citius/lab/joctomap/JOctreeIOTest.java
@@ -1,58 +1,76 @@
 package es.usc.citius.lab.joctomap;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import static org.junit.Assert.*;
 
 /**
- * Unit test for I/O over octrees.
+ * Unit test for I/O over octree objects. The {@link FixMethodOrder} policy
+ * ensures that the execution of the methods will be: read first, write last,
+ * avoiding errors due to the lack of ordering of the execution of the test
+ * cases in previous JUnit versions.
  */
-public class JOctreeIOTest{
-	
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class JOctreeIOTest {
+
 	/**
-	 * Load dynamic library.
+	 * Load dynamic library to access the native I/O methods.
 	 */
 	static {
 		System.loadLibrary("joctomap");
 	}
-	
-	private JOctree joctree;
-	
+
+	private File fileRead;
+	private File fileWrite;
+	private static final String testFilesDir = "src/test/resources/";
+	private static final String testFilesName = "fr_campus";
+
 	/**
 	 * Initialize data for I/O tests.
+	 * 
+	 * @throws IOException 
 	 */
-	public JOctreeIOTest() {
-		this.joctree = new JOctree();
+	@Before
+	public void initializeParams() throws IOException {
+		// get canonical files
+		this.fileRead = new File(testFilesDir.concat(testFilesName)
+				.concat(".ot")).getCanonicalFile();
+		this.fileWrite = new File(testFilesName.concat("_test").concat(".ot"))
+				.getCanonicalFile();
 	}
-	
-	/**
-	 * Unit test for reading a .bt file.
-	 */
-	@Test
-	public void readFileBt(){
-		joctree.read("bt file");
-	}
-	
+
 	/**
 	 * Unit test for reading a .ot file.
 	 */
 	@Test
-	public void readFileOt(){
-		joctree.read("ot file");
+	public void readFileOt() {
+		long octreeOt = JOctree.read(fileRead.getAbsolutePath());
+		assertTrue("Octree direction of memory is assigned: " + octreeOt,
+				octreeOt != 0);
 	}
-	
-	/**
-	 * Unit test for writing a .bt file.
-	 */
-	@Test
-	public void writeFileBt(){
-		joctree.write(0, "bt file");
-	}
-	
+
 	/**
 	 * Unit test for writing a .ot file.
+	 * 
+	 * @throws IOException 
 	 */
 	@Test
-	public void writeFileOt(){
-		joctree.write(0, "ot file");
+	public void writeFileOt() throws IOException {
+		//read ot
+		long octreeOt = JOctree.read(fileRead.getAbsolutePath());
+		//write file to compare sizes
+		JOctree.write(octreeOt, fileWrite.getAbsolutePath());
+		//compare by content both files
+		assertTrue("Input and output files with the same content",
+				FileUtils.contentEquals(fileRead, fileWrite));
+		//delete the generated file in this test case
+		fileWrite.delete();
 	}
-	
 }
