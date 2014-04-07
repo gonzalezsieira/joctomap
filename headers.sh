2f7a037d554dc710acabcfe1548ca0b77b8c23ab
Adrián González Sieira <adrian.gonzalez@usc.es>
2014-04-07 18:55:14 +0200

    Modified script to generate the JNI headers

--- headers.sh
+++ headers.sh
@@ -10,20 +10,22 @@ cd src/main/java/es/usc/citius/lab/joctomap
 javac JOctree.java
 
 # go to root of src
-cd "$dir"
-cd src/main/java
+cd "$dir"/src/main/java
 
 # generate JNI headers
 javah -jni es.usc.citius.lab.joctomap.JOctree
 
-# remove current headers and compiled files
+# remove current compiled files
 cd es/usc/citius/lab/joctomap
-rm *.h
 rm *.class
 
+# remove current headers
+cd "$dir"/src/main/resources/joctomap-natives/include
+rm *.h
+
 # move headers
-cd "$dir"/src/main/java
-mv *.h es/usc/citius/lab/joctomap/joctree.h
+cd "$dir/src/main/java"
+mv *JOctree.h ../resources/joctomap-natives/include/joctree.h
 
 # go to initial directory
 cd "$dir"
