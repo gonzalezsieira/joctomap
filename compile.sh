b534da00af7cf659b661c316c0e52b0a081ece85
Adrián González Sieira <adrian.gonzalez@usc.es>
2014-04-07 13:20:17 +0200

    compile.sh updated to avoid redundant code compilation

--- compile.sh
+++ compile.sh
@@ -4,20 +4,6 @@ echo -e "[exec] C/C++ build script begins"
 # save current directory
 dir=`pwd`
 
-# enter in resource directory to compile the library
-echo -e "[exec] Entering Octomap directory..."
-cd src/main/resources/joctomap-natives
-
-# octomap compile
-echo -e "[exec] Building Octomap..."
-mkdir build
-cd build
-cmake ..
-make
-
-# go to beginning directory
-cd "$dir"
-
 # enter in native packages to compile .c and .cpp sources
 cd src/main/resources/joctomap-natives
 
