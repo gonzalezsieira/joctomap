99d99be3ecff09221b280b3778365c3a6bf47362
Adrián González Sieira <adrian.gonzalez@usc.es>
2016-11-29 13:25:51 +0100

    Maven command now supports Release/Debug release types

--- compile.sh
+++ compile.sh
@@ -6,11 +6,23 @@ dir=`pwd`
 # enter in native packages to compile .c and .cpp sources
 cd joctomap-natives
 
-# compile
+# Start compiling
 echo "[exec] Building libjoctomap.so... (JAVA_JDK="$1")"
 mkdir build
 cd build
-cmake ../ -DJDK:STRING="$1"
+
+# Get release type
+release=$2
+if [ "$release" == "Debug" ]; then
+	echo "Release type: Debug"	
+	cmake ../ -DJDK:STRING="$1" -DCMAKE_BUILD_TYPE=Debug
+elif [ "$release" == "Release" ]; then
+	echo "Release type: Release" 
+	cmake ../ -DJDK:STRING="$1" -DCMAKE_BUILD_TYPE=Release
+else
+	echo "Release type not recognized, must be Release or Debug"
+	exit 1
+fi
 
 # Check failure (after cmake)
 result=$?
