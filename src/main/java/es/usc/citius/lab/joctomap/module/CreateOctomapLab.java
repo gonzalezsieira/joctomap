53e17d63edb518c1fb4551dea93c9ff5bae80933
Adrián González Sieira <adrian.gonzalez@usc.es>
2017-02-09 18:55:41 +0100

    changes in octomap for lab creation

--- src/main/java/es/usc/citius/lab/joctomap/module/CreateOctomapLab.java
+++ src/main/java/es/usc/citius/lab/joctomap/module/CreateOctomapLab.java
@@ -11,16 +11,20 @@ import org.apache.commons.cli.Options;
  */
 public class CreateOctomapLab extends Module {
 
-    static float size_box_x = 0.5f;
-    static float size_box_y = 1.0f;
-    static float resolution = 0.1f;
+    static float size_box_x = 0.35f;
+    static float size_box_y = 0.48f;
+    static float resolution = 0.03f;
     static Point2D min_position_flyarea = new Point2D(0, 0);
     static Point2D max_position_flyarea = new Point2D(5, 4);
     static Point2D min_position_map = new Point2D(-1, -1);
     static Point2D max_position_map = new Point2D(6, 5);
     static Point2D[] boxesCenter = new Point2D[]{
-            new Point2D(2, 2),
-            new Point2D( 3, 2)
+            new Point2D(0.49f, 0.44f),
+            new Point2D( 0.51f, 2.19f),
+            new Point2D(2.55f, 1.14f),
+            new Point2D(2.57f, 2.82f),
+            new Point2D(4.40f, 3.54f),
+            new Point2D(4.36f, 1.60f)
     };
 
     @Override
@@ -32,8 +36,8 @@ public class CreateOctomapLab extends Module {
     @Override
     public void execute(CommandLine args) {
         JOctree octree = JOctree.create(resolution);
-        for(float x = min_position_map.getX(); x <= max_position_map.getX(); x += resolution / 2f){
-            for(float y = min_position_map.getY(); y <= max_position_map.getY(); y += resolution / 2f){
+        for(float x = min_position_map.getX(); x <= max_position_map.getX(); x += resolution / 4f){
+            for(float y = min_position_map.getY(); y <= max_position_map.getY(); y += resolution / 4f){
                 boolean occupied = false;
                 // Check fly area
                 if (x < min_position_flyarea.getX() || x > max_position_flyarea.getX()
