734869cb8986c54772de76c4e11c1f5a1c276700
Adrián González Sieira <adrian.gonzalez@usc.es>
2017-05-08 18:30:16 +0200

    adapted headers script to new directory structure

--- headers.sh
+++ headers.sh
@@ -8,7 +8,7 @@ classpath=~/.m2/repository/es/usc/citius/lab/motionplanner-core/1.0.0-SNAPSHOT/m
 cd src/main/java/es/usc/citius/lab/joctomap/octree
 
 # compile files
-javac -cp "$classpath" ../util/JOctomapLogger.java ../util/AdjacencyMap.java ../util/Obstacle.java ../util/JOctreeUtils.java ../util/NativeUtils.java  ../util/NativeObject.java ../iterators/OctreeIterator.java ../iterators/LeafBBXIterator.java JOctree.java JOctreeKey.java JOctreeNode.java ../distance/JOctreeDistanceMap.java ../util/CollisionChecker2D.java ../hipster/H2DMRTransitionFunction.java
+javac -cp "$classpath" ../octree/Cell.java ../util/JOctomapLogger.java ../util/AdjacencyMap.java ../util/Obstacle.java ../util/JOctreeUtils.java ../util/NativeUtils.java  ../util/NativeObject.java ../iterators/OctreeIterator.java ../iterators/LeafBBXIterator.java JOctree.java JOctreeKey.java JOctreeNode.java ../distance/JOctreeDistanceMap.java ../util/CollisionChecker2D.java
 
 
 # go to root of src
@@ -22,7 +22,15 @@ javah -classpath "$classpath" -jni es.usc.citius.lab.joctomap.iterators.LeafBBXI
 javah -classpath "$classpath" -jni es.usc.citius.lab.joctomap.util.CollisionChecker2D
 javah -classpath "$classpath" -jni es.usc.citius.lab.joctomap.util.JOctreeUtils
 javah -classpath "$classpath" -jni es.usc.citius.lab.joctomap.util.AdjacencyMap
-javah -classpath "$classpath" -jni es.usc.citius.lab.joctomap.hipster.H2DMRTransitionFunction
+
+mv *JOctree.h ../../../joctomap-natives/include/joctree.h
+mv *JOctreeNode.h ../../../joctomap-natives/include/joctreenode.h
+mv *JOctreeDistanceMap.h ../../../joctomap-natives/include/joctreedistancemap.h
+mv *LeafBBXIterator.h ../../../joctomap-natives/include/leafbbxiterator.h
+mv *CollisionChecker2D.h ../../../joctomap-natives/include/collisionchecker2d.h
+mv *JOctreeUtils.h ../../../joctomap-natives/include/joctreeutils.h
+mv *AdjacencyMap.h ../../../joctomap-natives/include/adjacencymap.h
+rm *AdjacencyMap_Cache.h
 
 # remove current compiled files
 cd "$dir"/src/main/java/es/usc/citius/lab/joctomap/octree
@@ -33,20 +41,6 @@ cd "$dir"/src/main/java/es/usc/citius/lab/joctomap/distance
 rm *.class
 cd "$dir"/src/main/java/es/usc/citius/lab/joctomap/iterators
 rm *.class
-cd "$dir"/src/main/java/es/usc/citius/lab/joctomap/hipster
-rm *.class
-
-# move headers
-cd "$dir/src/main/java"
-mv *JOctree.h ../resources/joctomap-natives/include/joctree.h
-mv *JOctreeNode.h ../resources/joctomap-natives/include/joctreenode.h
-mv *JOctreeDistanceMap.h ../resources/joctomap-natives/include/joctreedistancemap.h
-mv *LeafBBXIterator.h ../resources/joctomap-natives/include/leafbbxiterator.h
-mv *CollisionChecker2D.h ../resources/joctomap-natives/include/collisionchecker2d.h
-mv *JOctreeUtils.h ../resources/joctomap-natives/include/joctreeutils.h
-mv *AdjacencyMap.h ../resources/joctomap-natives/include/adjacencymap.h
-mv *H2DMRTransitionFunction.h ../resources/joctomap-natives/include/h2dmrtransitionfunction.h
-rm *AdjacencyMap_Cache.h
 
 # go to initial directory
 cd "$dir"
