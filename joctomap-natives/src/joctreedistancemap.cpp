ccab3f6ed3f986f83cbdd0bb5ac09889415cfbc9
Adrián González Sieira <adrian.gonzalez@usc.es>
2017-05-09 17:55:21 +0200

    update headers info

--- joctomap-natives/src/joctreedistancemap.cpp
+++ joctomap-natives/src/joctreedistancemap.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014-2016 Adrián González Sieira (adrian.gonzalez@usc.es)
+ * Copyright (C) 2014-2017 Adrián González Sieira (adrian.gonzalez@usc.es)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
